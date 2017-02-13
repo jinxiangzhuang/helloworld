b1212613086f7b58608b78489f36b53ed1ef9471
jinxiangzhuang <616936018@qq.com>
2017-02-13 09:38:58 +0800

    Update hello.py
    增加判断条件

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print "hello world"
+receive_input = input("please input your number:") #获取用户输入（int型）
+if receive_input >=2: 
+    print "hello world"
+else:
+    print "happy Festival of Lanterns"
+
